b7df44df0b49eaf434dc4e560c7d6a38c6b213fa
Nils Hartmann <nils@nilshartmann.net>
2022-07-10 15:19:41 +0200

    PostEditorPage

--- blog-example/steps/10-context/src/App.tsx
+++ blog-example/steps/10-context/src/App.tsx
@@ -1,35 +1,20 @@
 import React from "react";
-import PostEditor from "./PostEditor";
-import { NewBlogPost } from "./types";
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import PostPage from "./PostPage";
 import PostListPage from "./PostListPage";
 import NotFoundPage from "./NotFound";
 import Page from "./Page";
 import AuthContextProvider from "./AuthContext";
+import PostEditorPage from "./PostEditorPage";
 
 function App() {
-  const navigate = useNavigate();
-  function savePost(post: NewBlogPost) {
-    fetch("http://localhost:7000/posts", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(post)
-    })
-      .then(response => response.json())
-      .then(_ => navigate("/"))
-      .catch(err => console.error("Saving failed: " + err));
-  }
-
   return (
     <AuthContextProvider>
       <Routes>
         <Route path="/" element={<Page />}>
           <Route index element={<PostListPage />} />
           <Route path="/post/:postId" element={<PostPage />} />
-          <Route path="/add" element={<PostEditor onSavePost={savePost} />} />
+          <Route path="/add" element={<PostEditorPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
--- blog-example/workspace-typescript-app/src/App.tsx
+++ blog-example/workspace-typescript-app/src/App.tsx
@@ -1,39 +1,27 @@
 import React from "react";
-import PostEditor from "./PostEditor";
-import { NewBlogPost } from "./types";
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import PostPage from "./PostPage";
 import PostListPage from "./PostListPage";
 import NotFoundPage from "./NotFound";
 import Page from "./Page";
+import PostEditorPage from "./PostEditorPage";
 
 function App() {
-  const navigate = useNavigate();
-  function savePost(post: NewBlogPost) {
-    fetch("http://localhost:7000/posts", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(post)
-    })
-      .then(response => response.json())
-      .then(_ => navigate("/"))
-      .catch(err => console.error("Saving failed: " + err));
-  }
-
   // TODO:
-  // - Wenn Du deinen AuthContextProvider implementiert hast,
-  //   Füge hier die AuthContextProvider als neue Top-Level-Komponente
-  //   hinzu
-  // - Implementieren dann 'CurrentUser'
+  // 1. Implementiere den AuthContextProvider (AuthContext.tsx)
+  // 2. Wenn Du deinen AuthContextProvider implementiert hast,
+  //    füge hier die AuthContextProvider als neue Top-Level-Komponente
+  //    hinzu
+  // 3. Implementiere dann die 'CurrentUser'-Komponente (CurrentUser.tsx)
+  // 4. Erweitere dann den PostEditor (PostEditor.tsx)
+  // (s. TODOs in den einzelnen Dateien)
 
   return (
     <Routes>
       <Route path="/" element={<Page />}>
         <Route index element={<PostListPage />} />
         <Route path="/post/:postId" element={<PostPage />} />
-        <Route path="/add" element={<PostEditor onSavePost={savePost} />} />
+        <Route path="/add" element={<PostEditorPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
