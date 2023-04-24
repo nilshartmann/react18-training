09732dc7cd8c7b7ae30aa6169b9f2bd111a352e6
Nils Hartmann <nils@nilshartmann.net>
2023-04-24 19:05:03 +0200

    React+TypeScript+Apollo Workshop

--- js-intro/ts/02_z_types_solution.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// Please ignore this line:
-exports.default = undefined;
-function createPerson({ name, age = 18 }) {
-    return { name, age };
-}
-const p = createPerson({ name: "Klaus" });
-// worin unterscheidet sich der hergeleitete Typ von p von Person
