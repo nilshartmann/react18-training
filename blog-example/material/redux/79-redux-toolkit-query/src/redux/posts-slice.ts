b7343484d8bfabe46212ac271f173d5a0a198f9f
Nils Hartmann <nils@nilshartmann.net>
2023-05-01 09:10:02 +0200

    move redux to material

--- blog-example/material/redux/79-redux-toolkit-query/src/redux/posts-slice.ts
+++ blog-example/material/redux/79-redux-toolkit-query/src/redux/posts-slice.ts
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { BlogPost, NewBlogPost } from "../types";
+import type { BlogPost, NewBlogPost } from "../types";
 
 export const postsApi = createApi({
   reducerPath: "posts",
